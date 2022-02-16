0240d4be37acf3f10cdd6bf7ce7dd53dc841ca5d
Joseph Harburg <josephharburg@gmail.com>
2022-02-16 10:11:19 -0500

    Added to do list to REST API controller and refactored survey object.

--- includes/class-virtue-survey-actions-and-shortcodes.php
+++ includes/class-virtue-survey-actions-and-shortcodes.php
@@ -10,7 +10,7 @@ class Virtue_Survey_Actions_And_Shortcodes
 {
   function __construct(){
     add_action( 'gform_after_submission_11', array($this, 'calculate_data_results'), 10, 2 );
-    add_action( 'gform_after_submission_12', array($this,'calculate_data_results'), 10, 2 );
+    add_action( 'gform_after_submission_12', array($this, 'calculate_data_results'), 10, 2 );
     add_shortcode( 'survey_results', array($this, 'output_survey_results'));
     add_shortcode( 'student_survey_results_table', array($this, 'output_student_table' ));
   }
@@ -19,29 +19,43 @@ class Virtue_Survey_Actions_And_Shortcodes
   * when we prerender the user_id_field it either uses the current users ID or generate a random one
   * in the form.
   */
+
   function calculate_data_results($entry, $form){
-    $user_id = rgar($entry, 19);
-    if($form['id'] == 12){
-      $user_id = get_current_user_id();
-    }
-    $user_results_meta_key = "$user_id-".rgar($entry,'id')."";
     if(class_exists('Virtue_Survey_Result')){
       $virtue_result_object = new Virtue_Survey_Result($entry);
     }
+    // If user is logged in add it to their account.
     if(is_user_logged_in()){
-      add_user_meta($user_id ,$user_results_meta_key, $virtue_result_object, true );
+      $user_id = get_current_user_id();
+      // Get the number of completed surveys
+      $survey_completions = get_user_meta( $user_id, "total_surveys_completed", true );
+      // We collect survey result objects individually as separate meta keys and values.
+      // Later, we will iterate through all the meta value results to calculate the
+      // increase or decrease of virute values.
+      if($survey_completions == '' || $survey_completions == false){
+        add_user_meta($user_id, "user_virtue_survey_result_1",$virtue_result_object, true);
+        add_user_meta($user_id, "total_surveys_completed", 1, true);
+      } else{
+        $survey_completions++;
+        add_user_meta($user_id, "user_virtue_survey_result_$survey_completions", $virtue_result_object, true);
+        update_user_meta($user_id, "total_surveys_completed", $survey_completions, $survey_completions--);
+      }
     } else{
+      $user_id = rgar($entry, 19);
+      $user_results_meta_key = "$user_id-".rgar($entry,'id')."";
       set_transient($user_results_meta_key, $virtue_result_object, DAY_IN_SECONDS );
     }
   }
 
   public function output_survey_results(){
-    $results_transient_key = $_GET['uid']. "-". $_GET['quiz-results'];
-    $results_array = get_transient( $results_transient_key );
     if(is_user_logged_in()){
-        $results = get_user_meta( get_current_user_id(), $results_transient_key, true );
+        $survey_completions = get_user_meta( $user_id, "total_surveys_completed", true);
+        $result_object = get_user_meta( get_current_user_id(), "user_virtue_survey_result_$survey_completions", true );
+        return $result_object::output_results_page($result_object->results);
     }
-    return $results::output_results_page($results->results);
+    $results_meta_key = $_GET['uid']. "-". $_GET['quiz-results'];
+    $result_object = get_transient( $results_meta_key );
+    return $result_object::output_results_page($result_object->results);
   }
 
    function output_student_table(){
--- includes/class-virtue-survey-rest-api-controller.php
+++ includes/class-virtue-survey-rest-api-controller.php
@@ -7,6 +7,19 @@ class Virtue_Survey_REST_API
 
   function __construct()
   {
-    // code...
+    // Add path to update Field Id Mapping By type
+
+    // Add path to retrive previous survey Versions
+
+    // Add path to Update Virtue Definition and Links (or have separate paths)?
   }
+
+
+// Add function to handle updating options table for each virtue
+
+// Add callback to handle downloading previous versions
+
+// Add callback to upload previous versions of survey
+
+// 
 }
