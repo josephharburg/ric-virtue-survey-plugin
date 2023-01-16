94738293204953b537c88e72b7c0c72f520788d3
Joseph Harburg <44932091+josephharburg@users.noreply.github.com>
2023-01-16 09:35:30 -0500

    Update settings to v2

--- includes/admin/class-virtue-survey-settings.php
+++ includes/admin/class-virtue-survey-settings.php
@@ -10,7 +10,7 @@ class Virtue_Survey_Settings
 {
     public function __construct(){
       add_action('admin_menu', array($this, 'vs_add_admin_menus'));
-      add_filter('plugin_action_links_'.VIRTUE_SURVEY_PLUGIN_NAME, array(__CLASS__, 'vs_plugin_settings_link'), 10, 1);
+      add_filter('plugin_action_links_'.VIRTUE_SURVEY_PLUGIN_NAME, array($this, 'vs_plugin_settings_link'), 10, 1);
       add_action('admin_enqueue_scripts', array($this, 'vs_enqueue_admin_scripts'));
       add_action('wp_enqueue_scripts', array($this, 'vs_enqueue_admin_scripts'));
     }
@@ -23,7 +23,7 @@ class Virtue_Survey_Settings
 
     public function vs_add_admin_menus(){
       $capability = 'create_users';
-      $plugin_icon ='data:image/svg+xml;base64,'. base64_encode(file_get_contents(VIRTUE_SURVEY_FILE_PATH.'/assets/fonts/file-icon.svg'));
+      $plugin_icon ='data:image/svg+xml;base64,'. base64_encode(file_get_contents(VIRTUE_SURVEY_PLUGIN_DIR_PATH.'/assets/fonts/file-icon.svg'));
 
       add_menu_page( 'Virtue Survey', 'Virtue Survey', $capability, 'virtue-survey-settings', array($this,'vs_settings_main'), $plugin_icon, 5);
 
@@ -81,12 +81,13 @@ class Virtue_Survey_Settings
 
     public function vs_enqueue_admin_scripts(){
       if(!is_admin()){
-        wp_enqueue_style( 'virtue-survey-fonts', VIRTUE_SURVEY_FILE_PATH .'assets/css/fonts.min.css', array());
+        wp_enqueue_style( 'virtue-survey-fonts', VIRTUE_SURVEY_FILE_PATH .'assets/css/fonts.css', array());
       return;
       }
       $current_css_ver  = date("ymd-Gis", filemtime(   VIRTUE_SURVEY_PLUGIN_DIR_PATH. 'assets/css/admin-styles.min.css'));
       wp_enqueue_style( 'virtue-survey-admin-styles', VIRTUE_SURVEY_FILE_PATH .'assets/css/admin-styles.min.css', array());
-      wp_enqueue_style( 'virtue-survey-fonts', VIRTUE_SURVEY_FILE_PATH .'assets/css/fonts.min.css', array());
+      wp_enqueue_style( 'virtue-survey-fonts', VIRTUE_SURVEY_FILE_PATH .'assets/css/fonts.css', array(),'2.0');
+
     }
 
     /**
